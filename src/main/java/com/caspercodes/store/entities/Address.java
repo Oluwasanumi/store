a8564163f551e13cd961b3fb76dc36a281573b2b
Oluwasanumi <oluwasanumibalogun@gmail.com>
2025-07-15 19:58:23 +0100

    Address Entity

---
+++ src/main/java/com/caspercodes/store/entities/Address.java
@@ -0,0 +1,34 @@
+package com.caspercodes.store.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "addresses")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "street", nullable = false)
+    private String street;
+
+    @Column(name = "city", nullable = false)
+    private String city;
+
+    @Column(name = "state", nullable = false)
+    private String state;
+
+    @Column(name = "zip", nullable = false)
+    private String zip;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+}
